5284f18a66d32cb3448f0a5716683bf38b577f13
javohotel <ja.ramirez@live.cl>
2017-02-17 09:23:25 -0400

    subiendo nuevos cambios sin efectos, falta arreglos de navbar

---
+++ nature/js/script.js
@@ -0,0 +1,25 @@
+$('html').csInit({
+
+  // global settings
+
+  wheelDelay: 300,
+
+  wheelDistance: 100,
+
+  wheelEase: 'easeOutQuad'
+
+   
+
+});
+
+
+
+
+
+<script type="text/javascript"> 
+
+    $(document).ready(function () { 
+        $('body').sectionScroll(); // Easy Peasy Lemon Squeezy 
+    }) 
+
+</script>
